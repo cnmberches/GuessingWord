2d11df5b1133c1ba10a49e882cedb2967e097097
GabgabLasconia12 <53455082+GabgabLasconia12@users.noreply.github.com>
2021-11-18 20:27:02 +0800

    Add files via upload

---
+++ GuessingWord/GuessingWord/Form1.Designer.cs
@@ -0,0 +1,328 @@
+﻿namespace GuessingWord
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.buttonA = new System.Windows.Forms.Button();
+            this.buttonN = new System.Windows.Forms.Button();
+            this.buttonG = new System.Windows.Forms.Button();
+            this.buttonE = new System.Windows.Forms.Button();
+            this.buttonR = new System.Windows.Forms.Button();
+            this.buttonCheck = new System.Windows.Forms.Button();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.countofWords = new System.Windows.Forms.Label();
+            this.labelLetters = new System.Windows.Forms.Label();
+            this.buttonD = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ScoreLBL = new System.Windows.Forms.Label();
+            this.buttonShuffle = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // buttonA
+            // 
+            this.buttonA.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(102)))), ((int)(((byte)(128)))), ((int)(((byte)(106)))));
+            this.buttonA.Cursor = System.Windows.Forms.Cursors.Default;
+            this.buttonA.FlatAppearance.BorderSize = 0;
+            this.buttonA.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonA.Font = new System.Drawing.Font("Poppins", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonA.ForeColor = System.Drawing.Color.White;
+            this.buttonA.Location = new System.Drawing.Point(29, 262);
+            this.buttonA.Name = "buttonA";
+            this.buttonA.Size = new System.Drawing.Size(57, 52);
+            this.buttonA.TabIndex = 1;
+            this.buttonA.Text = "A";
+            this.buttonA.UseVisualStyleBackColor = false;
+            this.buttonA.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // buttonN
+            // 
+            this.buttonN.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(102)))), ((int)(((byte)(128)))), ((int)(((byte)(106)))));
+            this.buttonN.Cursor = System.Windows.Forms.Cursors.Default;
+            this.buttonN.FlatAppearance.BorderSize = 0;
+            this.buttonN.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonN.Font = new System.Drawing.Font("Poppins", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonN.ForeColor = System.Drawing.Color.White;
+            this.buttonN.Location = new System.Drawing.Point(112, 262);
+            this.buttonN.Name = "buttonN";
+            this.buttonN.Size = new System.Drawing.Size(57, 52);
+            this.buttonN.TabIndex = 2;
+            this.buttonN.Text = "N";
+            this.buttonN.UseVisualStyleBackColor = false;
+            this.buttonN.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // buttonG
+            // 
+            this.buttonG.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(102)))), ((int)(((byte)(128)))), ((int)(((byte)(106)))));
+            this.buttonG.Cursor = System.Windows.Forms.Cursors.Default;
+            this.buttonG.FlatAppearance.BorderSize = 0;
+            this.buttonG.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonG.Font = new System.Drawing.Font("Poppins", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonG.ForeColor = System.Drawing.Color.White;
+            this.buttonG.Location = new System.Drawing.Point(197, 262);
+            this.buttonG.Name = "buttonG";
+            this.buttonG.Size = new System.Drawing.Size(57, 52);
+            this.buttonG.TabIndex = 3;
+            this.buttonG.Text = "G";
+            this.buttonG.UseVisualStyleBackColor = false;
+            this.buttonG.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // buttonE
+            // 
+            this.buttonE.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(102)))), ((int)(((byte)(128)))), ((int)(((byte)(106)))));
+            this.buttonE.Cursor = System.Windows.Forms.Cursors.Default;
+            this.buttonE.FlatAppearance.BorderSize = 0;
+            this.buttonE.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonE.Font = new System.Drawing.Font("Poppins", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonE.ForeColor = System.Drawing.Color.White;
+            this.buttonE.Location = new System.Drawing.Point(285, 262);
+            this.buttonE.Name = "buttonE";
+            this.buttonE.Size = new System.Drawing.Size(57, 52);
+            this.buttonE.TabIndex = 4;
+            this.buttonE.Text = "E";
+            this.buttonE.UseVisualStyleBackColor = false;
+            this.buttonE.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // buttonR
+            // 
+            this.buttonR.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(102)))), ((int)(((byte)(128)))), ((int)(((byte)(106)))));
+            this.buttonR.Cursor = System.Windows.Forms.Cursors.Default;
+            this.buttonR.FlatAppearance.BorderSize = 0;
+            this.buttonR.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonR.Font = new System.Drawing.Font("Poppins", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonR.ForeColor = System.Drawing.Color.White;
+            this.buttonR.Location = new System.Drawing.Point(368, 262);
+            this.buttonR.Name = "buttonR";
+            this.buttonR.Size = new System.Drawing.Size(57, 52);
+            this.buttonR.TabIndex = 5;
+            this.buttonR.Text = "R";
+            this.buttonR.UseVisualStyleBackColor = false;
+            this.buttonR.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // buttonCheck
+            // 
+            this.buttonCheck.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(102)))), ((int)(((byte)(128)))), ((int)(((byte)(106)))));
+            this.buttonCheck.Cursor = System.Windows.Forms.Cursors.Default;
+            this.buttonCheck.FlatAppearance.BorderSize = 0;
+            this.buttonCheck.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonCheck.Font = new System.Drawing.Font("Poppins", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonCheck.ForeColor = System.Drawing.Color.White;
+            this.buttonCheck.Location = new System.Drawing.Point(131, 359);
+            this.buttonCheck.Name = "buttonCheck";
+            this.buttonCheck.Size = new System.Drawing.Size(117, 39);
+            this.buttonCheck.TabIndex = 6;
+            this.buttonCheck.Text = "CHECK";
+            this.buttonCheck.UseVisualStyleBackColor = false;
+            this.buttonCheck.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(102)))), ((int)(((byte)(128)))), ((int)(((byte)(106)))));
+            this.buttonClear.Cursor = System.Windows.Forms.Cursors.Default;
+            this.buttonClear.FlatAppearance.BorderSize = 0;
+            this.buttonClear.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonClear.Font = new System.Drawing.Font("Poppins", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonClear.ForeColor = System.Drawing.Color.White;
+            this.buttonClear.Location = new System.Drawing.Point(313, 359);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(117, 39);
+            this.buttonClear.TabIndex = 7;
+            this.buttonClear.Text = "CLEAR";
+            this.buttonClear.UseVisualStyleBackColor = false;
+            this.buttonClear.Click += new System.EventHandler(this.button7_Click_1);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Poppins", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(112, 212);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(319, 35);
+            this.textBox2.TabIndex = 10;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Poppins", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(199, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(171, 34);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "WORD TO GUESS";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // countofWords
+            // 
+            this.countofWords.AutoSize = true;
+            this.countofWords.Font = new System.Drawing.Font("Poppins", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.countofWords.Location = new System.Drawing.Point(241, 79);
+            this.countofWords.Name = "countofWords";
+            this.countofWords.Size = new System.Drawing.Size(0, 84);
+            this.countofWords.TabIndex = 12;
+            this.countofWords.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // labelLetters
+            // 
+            this.labelLetters.AutoSize = true;
+            this.labelLetters.Font = new System.Drawing.Font("Poppins", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelLetters.Location = new System.Drawing.Point(178, 147);
+            this.labelLetters.Name = "labelLetters";
+            this.labelLetters.Size = new System.Drawing.Size(0, 62);
+            this.labelLetters.TabIndex = 13;
+            this.labelLetters.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // buttonD
+            // 
+            this.buttonD.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(102)))), ((int)(((byte)(128)))), ((int)(((byte)(106)))));
+            this.buttonD.Cursor = System.Windows.Forms.Cursors.Default;
+            this.buttonD.FlatAppearance.BorderSize = 0;
+            this.buttonD.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonD.Font = new System.Drawing.Font("Poppins", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonD.ForeColor = System.Drawing.Color.White;
+            this.buttonD.Location = new System.Drawing.Point(447, 262);
+            this.buttonD.Name = "buttonD";
+            this.buttonD.Size = new System.Drawing.Size(57, 52);
+            this.buttonD.TabIndex = 14;
+            this.buttonD.Text = "D";
+            this.buttonD.UseVisualStyleBackColor = false;
+            this.buttonD.Click += new System.EventHandler(this.buttonD_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Poppins", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(7, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 26);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "Score: ";
+            // 
+            // ScoreLBL
+            // 
+            this.ScoreLBL.AutoSize = true;
+            this.ScoreLBL.Font = new System.Drawing.Font("Poppins", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ScoreLBL.Location = new System.Drawing.Point(61, 9);
+            this.ScoreLBL.Name = "ScoreLBL";
+            this.ScoreLBL.Size = new System.Drawing.Size(22, 26);
+            this.ScoreLBL.TabIndex = 16;
+            this.ScoreLBL.Text = "0";
+            // 
+            // buttonShuffle
+            // 
+            this.buttonShuffle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(102)))), ((int)(((byte)(128)))), ((int)(((byte)(106)))));
+            this.buttonShuffle.Cursor = System.Windows.Forms.Cursors.Default;
+            this.buttonShuffle.FlatAppearance.BorderSize = 0;
+            this.buttonShuffle.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonShuffle.Font = new System.Drawing.Font("Poppins", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonShuffle.ForeColor = System.Drawing.Color.White;
+            this.buttonShuffle.Location = new System.Drawing.Point(131, 416);
+            this.buttonShuffle.Name = "buttonShuffle";
+            this.buttonShuffle.Size = new System.Drawing.Size(117, 39);
+            this.buttonShuffle.TabIndex = 17;
+            this.buttonShuffle.Text = "SHUFFLE";
+            this.buttonShuffle.UseVisualStyleBackColor = false;
+            this.buttonShuffle.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(102)))), ((int)(((byte)(128)))), ((int)(((byte)(106)))));
+            this.button2.Cursor = System.Windows.Forms.Cursors.Default;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Poppins", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(313, 416);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(117, 39);
+            this.button2.TabIndex = 18;
+            this.button2.Text = "EXIT";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(180)))), ((int)(((byte)(198)))), ((int)(((byte)(166)))));
+            this.ClientSize = new System.Drawing.Size(518, 467);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.buttonShuffle);
+            this.Controls.Add(this.ScoreLBL);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.buttonD);
+            this.Controls.Add(this.labelLetters);
+            this.Controls.Add(this.countofWords);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.buttonCheck);
+            this.Controls.Add(this.buttonR);
+            this.Controls.Add(this.buttonE);
+            this.Controls.Add(this.buttonG);
+            this.Controls.Add(this.buttonN);
+            this.Controls.Add(this.buttonA);
+            this.Name = "Form1";
+            this.Text = "GuessingWord";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button buttonA;
+        private System.Windows.Forms.Button buttonN;
+        private System.Windows.Forms.Button buttonG;
+        private System.Windows.Forms.Button buttonE;
+        private System.Windows.Forms.Button buttonR;
+        private System.Windows.Forms.Button buttonCheck;
+        private System.Windows.Forms.Button buttonClear;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label countofWords;
+        private System.Windows.Forms.Label labelLetters;
+        private System.Windows.Forms.Button buttonD;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label ScoreLBL;
+        private System.Windows.Forms.Button buttonShuffle;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ GuessingWord/GuessingWord/Form1.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace GuessingWord
+{
+    public partial class Form1 : Form
+    {
+        private int ticks = 180;
+        private int Score;
+        public Form1()
+        {
+
+            InitializeComponent();
+            timer1.Start();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox2.Text += "A";
+            inpput input = new inpput();   
+            input.inputWord.Append(textBox2.Text);
+           //MessageBox.Show(input.inputWord.ToString());
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox2.Text += "N";
+            inpput input = new inpput();
+            input.inputWord.Append(textBox2.Text);
+           //MessageBox.Show(input.inputWord.ToString());
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            char[] letter = { 'A', 'G', 'N', 'D', 'E', 'R' };
+            Random rand = new Random();
+            letter = letter.OrderBy(y => rand.Next()).ToArray();
+            foreach (char letters in letter)
+            {
+                
+                labelLetters.Text += letters + " ";
+            }
+            textBox2.Enabled = false;
+            MessageBox.Show("You have 180 seconds to guess a word, guess a word as many as you can! goods luck"); 
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textBox2.Text += "R";
+            inpput input = new inpput();
+            input.inputWord.Append(textBox2.Text);
+            //MessageBox.Show(input.inputWord.ToString());
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox2.Text += "E";
+            inpput input = new inpput();
+            input.inputWord.Append(textBox2.Text);
+          //  MessageBox.Show(input.inputWord.ToString());
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox2.Text += "G";
+            inpput input = new inpput();
+            input.inputWord.Append(textBox2.Text);
+            //MessageBox.Show(input.inputWord.ToString());
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void buttonD_Click(object sender, EventArgs e)
+        {
+            textBox2.Text += "D";
+            inpput input = new inpput();
+            input.inputWord.Append(textBox2.Text);
+            //MessageBox.Show(input.inputWord.ToString());
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button7_Click_1(object sender, EventArgs e)
+        {
+
+            textBox2.Text = String.Empty;
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+            
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            
+             Score = Convert.ToInt32(ScoreLBL.Text);
+            ArrayList word = new ArrayList();
+            word.Add("danger");
+            word.Add("range");
+            word.Add("red");
+            word.Add("age");
+            word.Add("gear");
+            word.Add("anger");
+            word.Add("aged");
+            word.Add("darn");
+            word.Add("dreg");
+            word.Add("gaen");
+            word.Add("grad");
+            word.Add("nerd");
+            word.Add("read");
+            word.Add("ager");
+            word.Add("dean");
+            word.Add("earn");
+            word.Add("gane");
+            word.Add("gran");
+            word.Add("rage");
+            word.Add("rend");
+            word.Add("dang");
+            word.Add("dear");
+            word.Add("egad");
+            word.Add("nard");
+            word.Add("rand");
+            word.Add("dare");
+            word.Add("drag");
+            word.Add("gnar");
+            word.Add("near");
+            word.Add("raged");
+            word.Add("rang");
+            word.Add("grade");
+            word.Add("redan");
+            word.Add("grand");
+            word.Add("regna");
+            word.Add("garden");
+            word.Add("ranged");
+            word.Add("dag");
+            word.Add("ran");
+            word.Add("and");
+            word.Add("era");
+            word.Add("are");
+            word.Add("ear");
+            word.Add("rag");
+         //   MessageBox.Show(word.Count.ToString());
+            inpput input = new inpput();
+
+            input.inputWord.Append(textBox2.Text);
+          //  MessageBox.Show(input.inputWord.ToString());
+            
+
+                if (word.Contains(input.inputWord.ToString().ToLower()))
+                {
+
+                    Score += 1;
+                    ScoreLBL.Text = Convert.ToString(Score);
+                    textBox2.Text = String.Empty;
+
+                }
+                else
+                {
+                    textBox2.Text = String.Empty;
+                MessageBox.Show("Invalid Word");
+                }
+            
+
+               
+            
+          
+
+            //comments
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            labelLetters.Text = String.Empty;
+            char[] letter = { 'A', 'G', 'N', 'D', 'E', 'R' };
+            Random rand = new Random();
+            letter = letter.OrderBy(y => rand.Next()).ToArray();
+            foreach (char letters in letter)
+            {
+                
+                labelLetters.Text += letters + " ";
+            }
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            System.Environment.Exit(0);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //MessageBox.Show(DateTime.Now.ToString());
+            // countofWords.Text = TimeSpan.Zero.TotalMinutes.ToString();
+            ticks--;
+           countofWords.Text = ticks.ToString();
+            if (ticks == 0)
+            {
+                countofWords.Text = "0";
+                timer1.Stop();
+                buttonA.Enabled = false;
+                buttonN.Enabled = false;
+                buttonD.Enabled = false;  
+                buttonE.Enabled = false;
+                buttonG.Enabled = false;
+                buttonR.Enabled = false;
+                buttonShuffle.Enabled = false;
+                buttonClear.Enabled = false;
+                buttonCheck.Enabled = false;
+                MessageBox.Show("Game over!" + Environment.NewLine + "Your total Score is: "+Score);
+            }
+        }
+    }
+
+       
+        
+
+    
+}
